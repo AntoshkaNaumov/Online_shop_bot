8a8ee9e1962d53e29168ccc0a135b74f8ff9bdaa
Anton Naumov <99544445+AntoshkaNaumov@users.noreply.github.com>
2023-08-13 15:00:20 +0000

    update

---
+++ data_base/sqlite_db.py
@@ -0,0 +1,48 @@
+import sqlite3 as sq
+from create_bot import bot
+
+
+def sql_start():
+    global base, cur
+    base = sq.connect('online_shop.db')
+    cur = base.cursor()
+    if base:
+        print('Data base connected OK!')
+    base.execute('CREATE TABLE IF NOT EXISTS shop(img TEXT, name PRIMARY KEY, description TEXT, price TEXT)')
+    base.execute('CREATE TABLE IF NOT EXISTS orders '
+                '(id INTEGER PRIMARY KEY AUTOINCREMENT, '
+                'user_id INTEGER, '
+                'order_number TEXT, '
+                'order_summary TEXT)')
+    base.commit()
+
+
+async def sql_add_command(state):
+    async with state.proxy() as data:
+        cur.execute('INSERT INTO shop VALUES (?, ?, ?, ?)', tuple(data.values()))
+        base.commit()
+
+
+async def sql_read():
+    products = []
+    for ret in cur.execute('SELECT * FROM shop').fetchall():
+        products.append(ret)
+    return products
+
+
+async def sql_read2():
+    return cur.execute('SELECT * FROM shop').fetchall()
+
+
+async def sql_delete_command(data):
+    cur.execute('DELETE FROM shop WHERE name == ?', (data,))
+    base.commit()
+
+
+async def get_product_by_name(product_name):
+    products = await sql_read()
+    for product in products:
+        photo, name, description, price = product
+        if name == product_name:
+            return product
+    return None
