d1b6fc3b4a9776221d280c94e727eaac54b5040a
Anton Naumov <99544445+AntoshkaNaumov@users.noreply.github.com>
2023-08-16 14:13:18 +0300

    Add files via upload

--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -7,19 +7,13 @@ b3 = KeyboardButton('/Каталог')
 b4 = KeyboardButton('/Регистрация')
 b5 = KeyboardButton('/Оплата')
 b6 = KeyboardButton('/Корзина')
+b7 = KeyboardButton('/Мои заказы')
 
 # Create a custom keyboard layout
 kb_client = ReplyKeyboardMarkup(resize_keyboard=True)
 
 
 # Add buttons to rows with custom text color
-kb_client.row(b3, b6, b1)
+kb_client.row(b3, b6, b7)
 kb_client.row(b4, b5, b2)
-
-
-#b7 = KeyboardButton('/Офоромить заказ')
-#b8 = KeyboardButton('/Мои заказы')
-
-#kb_client2 = ReplyKeyboardMarkup(resize_keyboard=True)
-
-#kb_client.add(b7).add(b8)
+kb_client.add(b1)
