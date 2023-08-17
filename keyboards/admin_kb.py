0d619a55c34020b059817a93978ede3613e1b62a
Anton Naumov <99544445+AntoshkaNaumov@users.noreply.github.com>
2023-08-17 13:32:04 +0300

    Add files via upload

--- keyboards/admin_kb.py
+++ keyboards/admin_kb.py
@@ -3,7 +3,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemo
 
 button_load = KeyboardButton('/Загрузить')
 button_delete = KeyboardButton('/Удалить')
-button_orders = KeyboardButton('/Посмотреть заказы')
+button_orders = KeyboardButton('/Просмотр')
 
 
 button_case_admin = ReplyKeyboardMarkup(resize_keyboard=True).add(button_load)\
