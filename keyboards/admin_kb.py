6efebe7b8bcbc1f1780ec6ff9534c07120232eeb
Anton Naumov <99544445+AntoshkaNaumov@users.noreply.github.com>
2023-08-24 18:43:05 +0300

    Add files via upload

--- keyboards/admin_kb.py
+++ keyboards/admin_kb.py
@@ -1,11 +1,21 @@
 # Кнопки клавиатуры админа
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.dispatcher import FSMContext
 
-button_load = KeyboardButton('/Загрузить')
-button_delete = KeyboardButton('/Удалить')
-button_orders = KeyboardButton('/Просмотр')
-button_loading = KeyboardButton('/Рассылка')
+
+button_load = KeyboardButton('/Download')
+button_delete = KeyboardButton('/Delete')
+button_orders = KeyboardButton('/Orders')
+button_loading = KeyboardButton('/Newsletter')
 
 
 button_case_admin = ReplyKeyboardMarkup(resize_keyboard=True).add(button_load)\
     .add(button_delete).add(button_orders).add(button_loading)
+
+
+# Create the submenu keyboard
+submenu_keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(
+    KeyboardButton('Confirmed Orders'),
+    KeyboardButton('Paid Orders'),
+    KeyboardButton('Back')
+)
