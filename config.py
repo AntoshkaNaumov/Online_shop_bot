b4b56342d4b7454d5bcd56ba74d591410b21485e
Anton Naumov <99544445+AntoshkaNaumov@users.noreply.github.com>
2023-09-09 15:19:43 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 key = '6516558262:AAERoE2KA_GtdyrKqPo_XUgAbFQTmxg6gDs'
 PAYMENT_TOKEN = '1744374395:TEST:54bdb5af7e1707c23f6f'
 GROUP_ADMIN_ID = '296318553'
-sec_key = 'live_T0wGPt3U-EFvNw0ecECEY-3Rjvsr_HrwBX7KtjB_kW4'
-secret_key = '390540012:LIVE:39614'
+sec_key = '390540012:LIVE:39614'
+secret_key = 'live_T0wGPt3U-EFvNw0ecECEY-3Rjvsr_HrwBX7KtjB_kW4'
