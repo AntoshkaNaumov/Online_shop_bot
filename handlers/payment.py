904dc7f2bb20555daa094ae17764d7c09ad4a806
Anton Naumov <99544445+AntoshkaNaumov@users.noreply.github.com>
2023-09-09 11:09:05 +0300

    Add files via upload

--- handlers/payment.py
+++ handlers/payment.py
@@ -1,13 +1,13 @@
 from aiogram import types, Dispatcher
 from create_bot import dp, bot
 import json
-from config import PAYMENT_TOKEN
+from config import PAYMENT_TOKEN, secret_key
 
 
 # @dp.message_handler(commands=['invoice'])  # подключение системы оплаты к боту
 async def invoice_test(message: types.Message):
     await bot.send_invoice(message.chat.id, 'Покупка товара',
-    'Покупка нашего лучшего товара', 'invoice', PAYMENT_TOKEN,
+    'Покупка нашего лучшего товара', 'invoice', secret_key,
     'RUB', [types.LabeledPrice('Покупка товара', 500 * 100)])
 
 
